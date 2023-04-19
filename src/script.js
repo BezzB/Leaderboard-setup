13f936668b12c9acc6104f30e78e4ee1c97356bb
BezzB <118984781+BezzB@users.noreply.github.com>
2023-04-19 23:58:26 +0300

    Update scripts.js

--- src/script.js
+++ src/script.js
@@ -1,19 +1,21 @@
+/* eslint-disable no-const-assign */
 import './style.css';
 
 // Store your API key and game ID in variables
-const API_KEY = 'Y5ifEX4zYPesLTPaTsGb8';
-let gameID;
+// eslint-disable-next-line no-unused-vars
+const API_KEY = 'h5tQF0UfCC2SNpraUeyL';
+const GAME_ID = 'h5tQF0UfCC2SNpraUeyL';
 
 // Function to fetch scores for your game from the API and update the table
 const refreshScores = async () => {
-  const response = await fetch(`https://api.leaderboard.com/games/${gameID}/scores?key=${API_KEY}`);
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GAME_ID}/scores/`);
   const data = await response.json();
   const tableBody = document.querySelector('.recentScores tbody');
   tableBody.innerHTML = '';
-  data.scores.forEach(score => {
+  data.result.forEach((score) => {
     const row = document.createElement('tr');
     row.innerHTML = `
-      <td>${score.name}</td>
+      <td>${score.user}</td>
       <td>${score.score}</td>
     `;
     tableBody.appendChild(row);
@@ -27,10 +29,10 @@ const addScore = async (event) => {
   const scoreInput = document.getElementById('score');
   const name = nameInput.value;
   const score = scoreInput.value;
-  const response = await fetch(`https://api.leaderboard.com/games/${gameID}/scores?key=${API_KEY}`, {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GAME_ID}/scores/`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name, score })
+    body: JSON.stringify({ user: name, score }),
   });
   if (response.ok) {
     nameInput.value = '';
@@ -41,13 +43,14 @@ const addScore = async (event) => {
 
 // Function to create a new game using the API
 const createGame = async () => {
-  const response = await fetch(`https://api.leaderboard.com/games?key=${API_KEY}`, {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name: 'YOUR_GAME_NAME' })
+    body: JSON.stringify({ name: 'YOUR_GAME_NAME' }),
   });
   const data = await response.json();
-  gameID = data.id;
+  // eslint-disable-next-line prefer-destructuring
+  GAME_ID = data.result.split('games/')[1].split('/')[0];
   refreshScores();
 };
 
